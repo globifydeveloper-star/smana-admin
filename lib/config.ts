a785e13008f6d48f30b77a22052a22e5229d099d
Arunlal <arunlal.m2000@gmail.com>
2026-01-31 14:31:08 +0530

    fix:env

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || (process.env.NODE_ENV === 'production' ? 'https://api.smanahotels.com' : 'http://localhost:5000');
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
 export const API_URL = `${BACKEND_URL}/api`;
